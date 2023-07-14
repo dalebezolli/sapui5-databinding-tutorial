a266d39e55947a2830bc4c79634da68fdc86b9e8
Dale Bezolli <deilbezolli@gmail.com>
2023-07-14 11:59:42 +0000

    Display user details using a button

--- databinding/webapp/controller/MainView.controller.js
+++ databinding/webapp/controller/MainView.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
 ],
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("sap.btp.databinding.controller.MainView", {
-            onInit: function () {
+            onDisplayUserDetails: function() {
+                const model = this.getOwnerComponent().getModel("user");
+                MessageToast.show(`Registering user with details\nfirstName=${model.getProperty("/firstName")}\nlastName=${model.getProperty("/lastName")}`);               
             }
         });
     });
