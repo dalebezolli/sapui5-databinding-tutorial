0fa955793d79ba6158a45585d8ac7c68f89bac22
Dale Bezolli <deilbezolli@gmail.com>
2023-07-14 11:17:00 +0000

    Create the greeting model and display the greeter from the model

--- databinding/webapp/controller/MainView.controller.js
+++ databinding/webapp/controller/MainView.controller.js
@@ -1,14 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/Text"
+    "sap/m/Text",
+    "sap/ui/model/json/JSONModel",
 ],
-    function (Controller, Text) {
+    function (Controller, Text, JSONModel) {
         "use strict";
 
         return Controller.extend("sap.btp.databinding.controller.MainView", {
             onInit: function () {
-                const page = this.getView().byId('page');
-                page.addContent(new Text({ text: 'Hi! My name is Dale' }));
+                const page = this.getView().byId("page");
+
+                page.addContent(new Text({ text: 'Hi {greeting>/greeter}!' }));
             }
         });
     });
