c40e0835b3781a264ce4159498aa5d69869485de
Dale Bezolli <deilbezolli@gmail.com>
2023-07-17 08:17:21 +0000

    Make send email open the email client

--- databinding/webapp/controller/MainView.controller.js
+++ databinding/webapp/controller/MainView.controller.js
@@ -1,14 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
+    "sap/m/library",
 ],
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, mobileLibrary) {
         "use strict";
 
         return Controller.extend("sap.btp.databinding.controller.MainView", {
             onDisplayUserDetails: function() {
                 const model = this.getOwnerComponent().getModel("user");
                 MessageToast.show(`Registering user with details\nfirstName=${model.getProperty("/firstName")}\nlastName=${model.getProperty("/lastName")}`);               
+            },
+            formatEmail: function(firstName, lastName) {
+                const resourceBundle = this.getView().getModel('i18n').getResourceBundle();
+
+                return mobileLibrary.URLHelper.normalizeEmail(
+                        firstName + '.' + lastName + '@example.com',
+                        resourceBundle.getText('mailSubject', [firstName]),
+                        resourceBundle.getText('mailBody')
+                        );
             }
         });
     });
